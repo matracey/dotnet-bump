36f10d9a35a192b18f56962a108bef4c990a6585
Martin Tracey <matracey@outlook.ie>
2024-03-17 03:56:05 -0700

    Add DotnetBump.Tool project

---
+++ src/DotnetBump.Tool/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Tool
+{
+    [Command(
+        Name = "ToolName",
+        FullName = "")]
+    [Subcommand("SampleCommand", typeof(SampleCommand))]
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandLineApplication<Program> { ThrowOnUnexpectedArgument = false };
+            app.Conventions.UseDefaultConventions().UseConstructorInjection();
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (CommandParsingException cpe)
+            {
+                Console.WriteLine(cpe.Message);
+                return -1;
+            }
+        }
+
+        protected int OnExecute(CommandLineApplication app)
+        {
+            // this shows help even if the --help option isn't specified
+            app.ShowHelp();
+            return 1;
+        }
+    }
+}
---
+++ src/DotnetBump.Tool/SampleCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Tool
+{
+    //Example Command
+    [Command(
+        Name = "dotnet samplecommand",
+        FullName = "dotnet-samplecommand")]
+    [HelpOption]
+    public class SampleCommand
+    {
+        //Example Argument
+        [Argument(0, ShowInHelpText = true, Name = "path", Description = "Path to the directory to execute this command.")]
+        public string Path { get; private set; }
+
+
+        public SampleCommand() 
+        {
+
+        }
+
+        public async Task<int> OnExecute(CommandLineApplication app)
+        {
+           
+
+            return await Task.FromResult(0);
+        }
+    }
+}
